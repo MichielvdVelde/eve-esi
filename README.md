0a46e94d09fb98b10ef346d701c4167428c826ec
Michiel van der Velde <michiel@michielvdvelde.nl>
2020-04-06 16:53:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# Eve Online ESI Client
+
+***Very much a work-in-progress!***
+
+Not published to npm yet. To use the module, clone the repository.
+
+This module makes use of my other Eve related module, [eve-sso](https://github.com/MichielvdVelde/eve-sso).
+Documentation is lacking, see the source code for guidance if you're brave enough
+to try it out.
+
+## Features
+
+* Make authenticated GET requests
+* Make authenticated POST requests
+* Automatically checks token validity/expiry and refreshes the access token when needed
+
+## Missing
+
+* Caching
+* Paging
+* And much more I'm sure
+
+## Example
+
+Or: how not to use this module.
+
+```ts
+import Koa from 'koa'
+import Router from 'koa-router'
+import jwt from 'jsonwebtoken'
+
+import SingleSignOn from 'eve-sso'
+import ESI from './index'
+
+// For the sake of this example
+// the character ID and class instance are defined here
+// In actual use you want to instantiate the class for each access token/character
+let characterId: number
+let esi: ESI
+
+// Application credentials for SSO
+const CLIENT_ID = 'clientId'
+const SECRET = 'secretKey'
+const CALLBACK_URI = 'callbackUri'
+
+const app = new Koa()
+const router = new Router()
+
+// Create an instance of eve-sso with requested scopes
+const sso = new SingleSignOn(CLIENT_ID, SECRET, CALLBACK_URI, {
+  scopes: 'esi-assets.read_assets.v1'
+})
+
+// Display a login link
+router.get('/', async ctx => {
+  ctx.body = `<a href="${sso.getRedirectUrl('my-state')}">Login Eve Online</a>`
+})
+
+// The SSO/oAuth callback url - this is defined in the developer section
+// and when instantiating eve-sso (see above)
+router.get('/sso', async ctx => {
+  const code: string = ctx.query.code
+  // Get an access token for the given code
+  const reply = await sso.getAccessToken(code)
+
+  const decoded = jwt.decode(reply.access_token) as { name: string, sub: string }
+
+  // Store the character ID
+  characterId = Number(decoded.sub.split(':').pop())
+  
+  // Create a class instance for this access token
+  esi = new ESI(sso, {
+    accessToken: reply.access_token,
+    refreshToken: reply.refresh_token
+  }, {
+    updateRefreshToken: async (oldToken, newToken) => {
+      // this method is called every time a new refresh token is issued
+      // use this to update your (database) records
+    }
+  })
+
+  // Redirect the user - to their assets, in this case
+  ctx.status = 302
+  ctx.res.setHeader('Location', '/assets')
+  ctx.body = `Logged in, ${decoded.name}! Redirecting...`
+})
+
+// Displays the logged in user's assets
+router.get('/assets', async ctx => {
+  if (!characterId || !esi) {
+    ctx.body = 'No character set!'
+    return
+  }
+
+  // Make an authenticated GET request to ESI
+  const reply = await esi.makeGetRequest<any>(
+    `/characters/${characterId}/assets/`
+  )
+
+  ctx.body = reply
+})
+
+app.use(router.middleware())
+app.listen(3001, () => {
+  console.log('Server listening on port 3001\n')
+})
+```
+
+## License
+
+Copyright 2020 Michiel van der Velde.
+
+This software is licensed under [the MIT License](LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,129 @@
+'use strict'
+
+import { stringify } from 'querystring'
+import { parse } from 'url'
+
+import bent from 'bent'
+import SingleSignOn from 'eve-sso'
+import jwt from 'jsonwebtoken'
+import formUrlencoded from 'form-urlencoded'
+
+const { name, version, homepage } = require('../package')
+
+export interface ESITokens {
+  refreshToken: string,
+  accessToken?: string
+}
+
+export interface ESIOptions {
+  updateRefreshToken?: (
+    oldRefreshToken: string,
+    newRefreshToken: string
+  ) => Promise<void>,
+  endpoint?: string,
+  userAgent?: string
+}
+
+export default class ESI {
+  public readonly sso: SingleSignOn
+  public readonly userAgent: string
+  public readonly endpoint: string
+
+  #accessToken?: string
+  #refreshToken: string
+
+  private updateRefreshToken: (
+    oldRefreshToken: string,
+    newRefreshToken: string
+  ) => Promise<void>
+
+  #getRequest: bent.RequestFunction<bent.Json>
+  #postRequest: bent.RequestFunction<bent.Json>
+
+  public constructor (
+    sso: SingleSignOn,
+    tokens: ESITokens,
+    opts: ESIOptions = {}
+  ) {
+    this.sso = sso
+    this.#refreshToken = tokens.refreshToken
+
+    if (tokens.accessToken) {
+      this.#accessToken = tokens.accessToken
+    }
+
+    this.endpoint = opts.endpoint || 'https://esi.evetech.net/latest'
+    this.userAgent = opts.userAgent || `${name}@${version} - nodejs@${process.version} - ${homepage}`
+
+    if (opts.updateRefreshToken) {
+      this.updateRefreshToken = opts.updateRefreshToken
+    }
+
+    const hostname = parse(this.endpoint).hostname
+
+    this.#getRequest = bent(this.endpoint, 'json', 'GET', {
+      Host: hostname,
+      'User-Agent': this.userAgent
+    })
+
+    this.#postRequest = bent(this.endpoint, 'json', 'POST', {
+      Host: hostname,
+      'User-Agent': this.userAgent
+    }, 200, 201)
+  }
+
+  public async makeGetRequest<T> (
+    uri: string,
+    query?: { [key: string]: any }
+  ): Promise<T> {
+    await this.checkAccessToken()
+
+    if (query) {
+      uri = `${uri}?${stringify(query)}`
+    }
+
+    return this.#getRequest(uri, null, {
+      Authorization: `Bearer ${this.#accessToken}`
+    }) as unknown as T
+  }
+
+  public async makePostRequest<T> (
+    uri: string,
+    payload: { [key: string]: any },
+    query?: { [key: string]: any }
+  ): Promise<T> {
+    await this.checkAccessToken()
+
+    if (query) {
+      uri = `${uri}?${stringify(query)}`
+    }
+
+    return this.#postRequest(uri, formUrlencoded(payload), {
+      Authorization: `Bearer ${this.#accessToken}`
+    }) as unknown as T
+  }
+
+  private async checkAccessToken (): Promise<void> {
+    let refreshAccessToken = false
+
+    if (!this.#accessToken) {
+      refreshAccessToken = true
+    } else {
+      const { exp } = jwt.decode(this.#accessToken) as { exp: number }
+      refreshAccessToken = new Date().getTime() > exp
+    }
+
+    if (refreshAccessToken) {
+      const reply = await this.sso.getAccessToken(this.#refreshToken, true)
+      this.#accessToken = reply.access_token
+
+      if (this.#refreshToken !== reply.refresh_token) {
+        if (this.updateRefreshToken) {
+          await this.updateRefreshToken(this.#refreshToken, reply.refresh_token)
+        }
+
+        this.#refreshToken = reply.refresh_token
+      }
+    }
+  }
+}
